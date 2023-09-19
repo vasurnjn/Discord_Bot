e4afeeb3cb2088b074a8bd8479db50a10b45bce6
Glitch (glitch-hello-node) <none>
2023-09-19 19:42:17 +0000

    😇⛴ Checkpoint ./server.js:2415070/984

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 
-
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
 
 const express = require('express')
 const app = express()
@@ -13,4 +15,28 @@ app.get('/', function(req, res) {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+client.on('message', message => {
+  
+  (async () => {
+	const readdir = require('util').promisify(fs.readdir)
+	const categories = await readdir('./src/commands/')
+	categories.forEach(category => {
+		if (category.endsWith('.js')) return;
+		const commandFiles = fs.readdirSync(`./src/commands/${category}/`).filter(file => file.endsWith('.js'))
+		for (const file of commandFiles) {
+			const command = require(`./src/commands/${category}/${file}`)
+			if (!command || !command.info || !command.info.name || !command.execute) console.log('[Crysto] Error in file ' + file + '! File not loaded.')
+				command.info.module = category
+			client.commands.set(command.info.name, command)
+		}
+		client.modules.push(category)
+		console.log(`[Crysto] Loaded ${category} commands`)
+	})
+})()
+
+  
+
+  
 })

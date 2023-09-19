2f37d40f2e79cd2cd6019a8f9d9b1be41c0de696
Glitch (glitch-hello-node) <none>
2023-09-19 20:17:22 +0000

    🏆🍲 Checkpoint ./server.js:2415070/2311:61527360/744 ./package.json:2415070/2019 ./watch.json:2415070/242

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const client = new Discord.Client({
+  intents: ['Guilds', 'GuildMessages', 'MessageContent']
+})
 const fs = require('fs')
 
 const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-
+const dotenv = require('dotenv')
+dotenv.config()
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -17,26 +20,24 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-client.on('message', message => {
+client.on('messageCreate', message => {
   
-  (async () => {
-	const readdir = require('util').promisify(fs.readdir)
-7	const categories = await readdir('./src/commands/')
-	categories.forEach(category => {
-		if (category.endsWith('.js')) return;
-		const commandFiles = fs.readdirSync(`./src/commands/${category}/`).filter(file => file.endsWith('.js'))
-		for (const file of commandFiles) {
-			const command = require(`./src/commands/${category}/${file}`)
-			if (!command || !command.info || !command.info.name || !command.execute) console.log('[Crysto] Error in file ' + file + '! File not loaded.')
-				command.info.module = category
-			client.commands.set(command.info.name, command)
-		}
-		client.modules.push(category)
-		console.log(`[Crysto] Loaded ${category} commands`)
-	})
-})()
+  let msg = message.content
+  if (message.author.bot) return;
+  let prefix = 'c!'
 
+  let args = message.content.slice(prefix.length).split(/ +/g);
+  const commandName = args.shift().toLowerCase()
+  
+  if (commandName === 'info') {
+    message.reply("TEST")
+  }
+  if (commandName === 'weather') {
+    
+  }
   
 
   
 })
+
+client.login(process.env.BOT_TOKEN)

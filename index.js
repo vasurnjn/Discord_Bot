45bab77cadae33d5185dcfb500e85be64c89b118
Glitch (glitch-hello-node) <none>
2023-09-19 21:51:55 +0000

    💿🎌 Checkpoint ./index.js:2415070/4638 ./style.css:2415070/11532 ./index.html:2415070/6754 ./watch.json:2415070/10 ./server.js:2415070/4

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const registrationForm = document.getElementById('registration-form');
+    const loginForm = document.getElementById('login-form');
 
-document.getElementById("b1").innerHTML = "Hello JavaScript!";
+    registrationForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const username = document.getElementById('reg-username').value;
+        const email = document.getElementById('reg-email').value;
+        const password = document.getElementById('reg-password').value;
+  
+        registrationForm.reset();
+    });
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const username = document.getElementById('login-username').value;
+        const password = document.getElementById('login-password').value;
+        loginForm.reset();
+    });
+});
